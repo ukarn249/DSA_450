f33d4376e582cf4b3215727771dc52d31c4db78f
Ujjawal Karn <ukarn124@gmail.com>
2022-06-21 12:57:57 +0530

    Create 1_reverse_array.cpp

---
+++ 1_reverse_array.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+void reverseArray(int arr[], int start, int end){
+    if(start>=end){
+        return;
+    }
+    int temp = arr[start];
+    arr[start] = arr[end];
+    arr[end] = temp;
+    reverseArray(arr, start+1, end-1);
+}
+void printArray(int arr[], int size){
+    for(int i=0;i<size;i++){
+        cout<<arr[i]<<" ";
+    }
+}
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+    printArray(arr, n);
+    reverseArray(arr, 0, n-1);
+    cout<<"\nAfter reverse: ";
+    printArray(arr, n);
+    return 0;
+
+}
